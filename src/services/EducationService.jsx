3e2495366e3122ff725deeed9ecc1a32205db1a1
shivabhujbal <shivsambh2001bhujbal@gmail.com>
2024-08-08 10:13:05 +0530

    service folder added

---
+++ src/services/EducationService.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+const BASE_URL = 'http://localhost:8080/user';
+
+
+const addEducation = async(educationData) =>{
+    try{
+        const response = await axios.post(`${BASE_URL}/addEducation`,educationData);
+        return response.data
+    }
+    catch(error){
+        console.error("Error While adding Education",error);
+        throw error;
+
+        
+    }
+}
+
+const getEducation = async(userId) =>{
+
+    try {
+        const response =await axios.get(`${BASE_URL}/${userId}/getEducation`);
+        return response.data
+    } catch (error) {
+
+        console.error("Error While getting Education",error);
+        throw error;
+        
+        
+    }
+}
+
+export{addEducation,getEducation};
