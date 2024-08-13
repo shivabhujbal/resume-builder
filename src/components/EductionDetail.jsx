46097a5adf43e9a16e8fa3cb19a85ac3a799fd94
shivabhujbal <shivsambh2001bhujbal@gmail.com>
2024-08-13 17:18:25 +0530

    Education updated

--- src/components/EductionDetail.jsx
+++ src/components/EductionDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { addEducation } from '../services/EducationService';
 
 function EducationDetail() {
   const [formData, setFormData] = useState({
@@ -9,60 +10,55 @@ function EducationDetail() {
     schoolLocation: '',
     degree: '',
     fieldOfStudy: '',
-    graduationMonth: '',
+    month: '',
     graduationYear: '',
-    userId:'',
+    userId: '1',
+    isGapTaken: false,
+    gapYear: '', 
+    certifications: [],
+
   });
 
-  const [errors, setErrors] = useState({});
 
- const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
+
+const [newCertification, setNewCertification] = useState(''); 
+const [errors, setErrors] = useState({});
+
+  const months = [
+    'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER',
   ];
-
   const degrees = [
-    { value: "X", label: "12th (HSC)" },
-    { value: "XI", label: "10th (SSC)" },
-    { value: "BCA", label: "Bachelor of Computer Application (BCA)" },
-    { value: "BBA", label: "Bachelor of Business Administration (BBA)" },
-    { value: "BCom", label: "Bachelor of Commerce (BCom)" },
-    { value: "BSc", label: "Bachelor of Science (BSc)" },
-    { value: "BA", label: "Bachelor of Arts (BA)" },
-    { value: "BE", label: "Bachelor of Engineering (BE)" },
-    { value: "BTech", label: "Bachelor of Technology (BTech)" },
-    { value: "BEd", label: "Bachelor of Education (BEd)" },
-    { value: "BFA", label: "Bachelor of Fine Arts (BFA)" },
-    { value: "LLB", label: "Bachelor of Law (LLB)" },
-    { value: "MBBS", label: "Bachelor of Medicine (MBBS)" },
-    { value: "MCA", label: "Master of Computer Application (MCA)" },
-    { value: "MBA", label: "Master of Business Administration (MBA)" },
-    { value: "MCom", label: "Master of Commerce (MCom)" },
-    { value: "MSc", label: "Master of Science (MSc)" },
-    { value: "MA", label: "Master of Arts (MA)" },
-    { value: "ME", label: "Master of Engineering (ME)" },
-    { value: "MTech", label: "Master of Technology (MTech)" },
-    { value: "MEd", label: "Master of Education (MEd)" },
-    { value: "MFA", label: "Master of Fine Arts (MFA)" },
-    { value: "LLM", label: "Master of Law (LLM)" },
-    { value: "MPH", label: "Master of Public Health (MPH)" },
-    { value: "MSW", label: "Master of Social Work (MSW)" },
-    { value: "PhD", label: "Doctor of Philosophy (PhD)" },
-    { value: "MD", label: "Doctor of Medicine (MD)" },
-    { value: "DBA", label: "Doctor of Business Administration (DBA)" },
-    { value: "EdD", label: "Doctor of Education (EdD)" },
-    { value: "DrPH", label: "Doctor of Public Health (DrPH)" }
-  ];
+    { value: "BACHELOR_OF_COMPUTER_APPLICATION", label: "Bachelor of Computer Application (BCA)" },
+    { value: "BACHELOR_OF_BUSINESS_ADMINISTRATION", label: "Bachelor of Business Administration (BBA)" },
+    { value: "BACHELOR_OF_COMMERCE", label: "Bachelor of Commerce (BCom)" },
+    { value: "BACHELOR_OF_SCIENCE", label: "Bachelor of Science (BSc)" },
+    { value: "BACHELOR_OF_ARTS", label: "Bachelor of Arts (BA)" },
+    { value: "BACHELOR_OF_ENGINEERING", label: "Bachelor of Engineering (BE)" },
+    { value: "BACHELOR_OF_TECHNOLOGY", label: "Bachelor of Technology (BTech)" },
+    { value: "BACHELOR_OF_EDUCATION", label: "Bachelor of Education (BEd)" },
+    { value: "BACHELOR_OF_FINE_ARTS", label: "Bachelor of Fine Arts (BFA)" },
+    { value: "BACHELOR_OF_LAW", label: "Bachelor of Law (LLB)" },
+    { value: "BACHELOR_OF_MEDICINE", label: "Bachelor of Medicine (MBBS)" },
+    { value: "MASTER_OF_COMPUTER_APPLICATION", label: "Master of Computer Application (MCA)" },
+    { value: "MASTER_OF_BUSINESS_ADMINISTRATION", label: "Master of Business Administration (MBA)" },
+    { value: "MASTER_OF_COMMERCE", label: "Master of Commerce (MCom)" },
+    { value: "MASTER_OF_SCIENCE", label: "Master of Science (MSc)" },
+    { value: "MASTER_OF_ARTS", label: "Master of Arts (MA)" },
+    { value: "MASTER_OF_ENGINEERING", label: "Master of Engineering (ME)" },
+    { value: "MASTER_OF_TECHNOLOGY", label: "Master of Technology (MTech)" },
+    { value: "MASTER_OF_EDUCATION", label: "Master of Education (MEd)" },
+    { value: "MASTER_OF_FINE_ARTS", label: "Master of Fine Arts (MFA)" },
+    { value: "MASTER_OF_LAW", label: "Master of Law (LLM)" },
+    { value: "MASTER_OF_PUBLIC_HEALTH", label: "Master of Public Health (MPH)" },
+    { value: "MASTER_OF_SOCIAL_WORK", label: "Master of Social Work (MSW)" },
+    { value: "DOCTOR_OF_PHILOSOPHY", label: "Doctor of Philosophy (PhD)" },
+    { value: "DOCTOR_OF_MEDICINE", label: "Doctor of Medicine (MD)" },
+    { value: "DOCTOR_OF_BUSINESS_ADMINISTRATION", label: "Doctor of Business Administration (DBA)" },
+    { value: "DOCTOR_OF_EDUCATION", label: "Doctor of Education (EdD)" },
+    { value: "DOCTOR_OF_PUBLIC_HEALTH", label: "Doctor of Public Health (DrPH)" },
+    { value: "other", label: "Other" }
+];
+
 
   const validate = () => {
     const newErrors = {};
@@ -79,12 +75,61 @@ function EducationDetail() {
     if (!formData.fieldOfStudy) newErrors.fieldOfStudy = "Field of study is required.";
     if (formData.schoolName.length < 3) newErrors.schoolName = "Field of study must be at least 3 characters long.";
 
-    if (!formData.graduationMonth) newErrors.graduationMonth = "Graduation month is required.";
+    if (!formData.month) newErrors.month = "Graduation month is required.";
     if (!formData.graduationYear) newErrors.graduationYear = "Graduation Year is required.";
+
+
+    if (formData.isGapTaken && !formData.gapYear) {
+      newErrors.gapYear = "Gap years is required."; // Add this line
+    }
+
+    if (formData.isGapTaken && !formData.gapYear) {
+      newErrors.gapYear = "Gap years is required."; // Add this line
+    }
+
     
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
+
+// Handle adding a new certification to the list
+
+  const handleCertificationChange = (e) => {
+    setNewCertification(e.target.value);
+  };
+
+  const handleAddCertification = () => {
+    if (newCertification.trim() !== '') {
+      setFormData((prevData) => ({
+        ...prevData,
+        certifications: [...prevData.certifications, newCertification],
+      }));
+      setNewCertification(''); // Clear the input field after adding
+    }
+  };
+
+  const handleRemoveCertification = (index) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      certifications: prevData.certifications.filter((_, i) => i !== index),
+    }));
+  };
+
+
+// ====================================================
+
+
+  const handleCheckboxChange =(e)=>{
+    const{checked} =e.target;
+    setFormData((prevData) =>({
+      ...prevData,
+      isGapTaken:checked,
+      gapYear:checked ? prevData.gapYear:'',
+    })
+  
+  )
+
+  }
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -103,25 +148,31 @@ function EducationDetail() {
     e.preventDefault();
     if (validate()) {
       console.log(formData);
-      // Handle form submission
+
     }
   };
-  const handleAddEducation = (e) => {
+  const handleAddEducation = async(e) => {
     e.preventDefault();
     if (validate()) {
       console.log(formData);
 
+      const response = await addEducation(formData);
+      console.log("res",response);
+      
+
+      
+
       alert("Education Details Submitted")
 
-      setFormData({
-        schoolName: '',
-        schoolLocation: '',
-        degree: '',
-        fieldOfStudy: '',
-        graduationMonth: '',
-        graduationYear: '',
-        userId:'',
-      });
+      // setFormData({
+      //   schoolName: '',
+      //   schoolLocation: '',
+      //   degree: '',
+      //   fieldOfStudy: '',
+      //   month: '',
+      //   graduationYear: '',
+       
+      // });
 
       
     }
@@ -232,10 +283,10 @@ function EducationDetail() {
                   </label>
                   <div className="grid grid-cols-2 gap-4">
                     <select
-                      name="graduationMonth"
-                      value={formData.graduationMonth}
+                      name="month"
+                      value={formData.month}
                       onChange={handleChange}
-                      className={`mt-1 block w-full px-3 py-2 bg-white border rounded-sm shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${errors.graduationMonth ? 'border-red-500' : 'border-gray-300'}`}
+                      className={`mt-1 block w-full px-3 py-2 bg-white border rounded-sm shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${errors.month ? 'border-red-500' : 'border-gray-300'}`}
                     >
                       <option value="">Month</option>
                       {months.map((month, index) => (
@@ -261,10 +312,84 @@ function EducationDetail() {
                       })}
                     </select>
                   </div>
-                  {errors.graduationMonth && <p className="text-red-500 text-sm">{errors.graduationMonth}</p>}
+                  {errors.month && <p className="text-red-500 text-sm">{errors.month}</p>}
                   {errors.graduationYear && <p className="text-red-500 text-sm">{errors.graduationYear}</p>}
                 </div>
               </div>
+
+              <div className="col-span-2 sm:col-span-1">
+  <label className="block mb-1 font-semibold text-gray-700">
+    Gap Year(s) Available
+  </label>
+  <input
+    type="checkbox"
+    name="isGapTaken"
+    checked={formData.isGapTaken}
+    onChange={handleCheckboxChange}
+    className="mr-2 leading-tight"
+  />
+</div>
+
+{formData.isGapTaken=="1" && (
+  <div className="col-span-2 sm:col-span-1">
+    <label className="block mb-1 font-semibold text-gray-700">
+      Gap Year(s) Duration<span className="text-red-900">*</span>
+    </label>
+    <input
+      type="text"
+      name="gapYear"
+      placeholder="e.g. 2020-2021"
+      value={formData.gapYear}
+      onChange={handleChange}
+      className={`mt-1 block w-full px-3 py-2 bg-white border rounded-sm shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${errors.gapYear ? 'border-red-500' : 'border-gray-300'}`}
+    />
+    {errors.gapYear && <p className="text-red-500 text-sm">{errors.gapYear}</p>}
+  </div>
+)}
+
+{/* Certifications */}
+<div className="col-span-2 sm:col-span-1">
+          <label className="block mb-1 font-semibold text-gray-700">
+            Certifications
+          </label>
+          <div className="flex space-x-2">
+            <input
+              type="text"
+              name="certifications"
+              placeholder="e.g. Certified Java Developer"
+              value={newCertification}
+              onChange={handleCertificationChange}
+              className={`mt-1 block w-full px-3 py-2 bg-white border rounded-sm shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${errors.certifications ? 'border-red-500' : 'border-gray-300'}`}
+            />
+            <button
+              type="button"
+              onClick={handleAddCertification}
+              className="py-2 px-4 bg-blue-600 text-white rounded-sm"
+            >
+              Add
+            </button>
+          </div>
+          {/* Display the list of added certifications */}
+          {formData.certifications.length > 0 && (
+            <ul className="mt-2">
+              {formData.certifications.map((cert, index) => (
+                <li key={index} className="flex justify-between items-center">
+                  <span>{cert}</span>
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveCertification(index)}
+                    className="text-red-500 text-sm"
+                  >
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+
+
+
 
               <div className="mt-10 pr-5 pb-10 flex justify-end space-x-5">
                 <button
