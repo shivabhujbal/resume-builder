3c76e12d51818c7ce75932437052e7d1076658f1
shivabhujbal <shivsambh2001bhujbal@gmail.com>
2024-08-16 15:22:33 +0530

    added userData

---
+++ src/resumetemplates/UserDetails.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { getAllDetails } from '../services/UserData';
+
+const UserDetails = () => {                     // use id as a props {id}
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+   
+    const fetchUserData = async () => {
+        const response =await getAllDetails(1);
+        setUserData(response);  
+    }
+
+    fetchUserData()
+
+  });
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="container mt-5">
+
+        <h1>User Data is Fetched</h1>
+ 
+  </div>
+  );
+};
+
+export default UserDetails;
