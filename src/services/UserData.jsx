e9c6de99c31dd2caa441212c406f27e327b11736
shivabhujbal <shivsambh2001bhujbal@gmail.com>
2024-08-16 15:21:32 +0530

    added userData

---
+++ src/services/UserData.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+const BASE_URL = 'http://localhost:8080/user';
+
+
+const getAllDetails = async(userId) =>{
+
+
+    try {
+        const response =await axios.get(`${BASE_URL}/getAllBasicDetails/${userId}`);
+        return response.data
+    } catch (error) {
+
+        console.error("Error While getting All user Data",error);
+        throw error;
+        
+        
+    }
+
+
+
+    
+}
+
+
+export{getAllDetails}
